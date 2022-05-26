479e750c06350a3990e08726ef4ccab18659814b
nlkennengounou <84591282+nlkennengounou@users.noreply.github.com>
2022-05-26 20:20:42 +0200

    NEW TEST: Ajout des nouveaux tests integration pour couvrir les tests lors du chargement des données via l'api

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -7,6 +7,7 @@ import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
 import { ROUTES_PATH} from "../constants/routes.js";
 import {localStorageMock} from "../__mocks__/localStorage.js";
+import store from "../__mocks__/store"
 
 import router from "../app/Router.js";
 import win from "global";
@@ -116,5 +117,72 @@ describe("Given I am connected as an employee", () => {
       expect(document.getElementById('modaleFile')).toBeTruthy()
     })
   })
+  
+})
 
+
+// Ajouter un test d'intégration GET Bills.
+describe('Given I am user connected as employee',()=>{
+  describe('When I navigate to Bill page',()=>{
+    test("fetches bills from mock API GET", async () => {
+      localStorage.setItem("user", JSON.stringify({ type: "Employee", email: "a@a" }));
+      const root = document.createElement("div")
+      root.setAttribute("id", "root")
+      document.body.append(root)
+      router()
+      window.onNavigate(ROUTES_PATH.Bills)
+    
+      const contentPending = await screen.getByText("Mes notes de frais")
+      expect(contentPending).toBeTruthy()
+      expect(screen.getByTestId("btn-new-bill")).toBeTruthy()
+    })
+
+    describe("When an error occurs on API", () => {
+     beforeEach(() => {
+       jest.spyOn(store, "bills")
+       Object.defineProperty(
+           window,
+           'localStorage',
+           { value: localStorageMock }
+       )
+       window.localStorage.setItem('user', JSON.stringify({
+         type: 'Employee',
+         email: "a@a"
+       }))
+       const root = document.createElement("div")
+       root.setAttribute("id", "root")
+       document.body.appendChild(root)
+       router()
+     })
+     test("fetches bills from an API and fails with 404 message error", async () => {
+ 
+       store.bills.mockImplementationOnce(() => {
+         return {
+           list : () =>  {
+             return Promise.reject(new Error("Erreur 404"))
+           }
+         }})
+         const html = BillsUI({error: "Erreur 404"})
+         document.body.innerHTML = html
+         const message = await screen.getByText(/Erreur 404/)
+         expect(message).toBeTruthy()
+     })
+ 
+     test("fetches messages from an API and fails with 500 message error", async () => {
+ 
+       store.bills.mockImplementationOnce(() => {
+         return {
+           list : () =>  {
+             return Promise.reject(new Error("Erreur 500"))
+           }
+         }})
+ 
+
+       const html = BillsUI({error: "Erreur 500"})
+       document.body.innerHTML = html
+       const message = await screen.getByText(/Erreur 500/)
+       expect(message).toBeTruthy()
+     })
+   })
+  })
 })
