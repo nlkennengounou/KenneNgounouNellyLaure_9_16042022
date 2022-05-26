cb2e331311c317ce64dd4cab63302774c15879f7
nlkennengounou <84591282+nlkennengounou@users.noreply.github.com>
2022-05-26 19:05:50 +0200

    NEW TEST: Ajout des nouveaux tests unitaires pour couvrir les tests modal, new bill, et error and loading page

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -11,6 +11,8 @@ import {localStorageMock} from "../__mocks__/localStorage.js";
 import router from "../app/Router.js";
 import win from "global";
 
+import Bills from "../containers/Bills"
+
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
     test("Then bill icon in vertical layout should be highlighted", async () => {
@@ -31,6 +33,7 @@ describe("Given I am connected as an employee", () => {
       expect(windowIcon.classList.contains("active-icon")).toBe(true)
     })
     test("Then bills should be ordered from earliest to latest", () => {
+      // build user interface
       document.body.innerHTML = BillsUI({ data: bills })
       const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
       const antiChrono = (a, b) => ((a < b) ? 1 : -1)
@@ -38,4 +41,80 @@ describe("Given I am connected as an employee", () => {
       expect(dates).toEqual(datesSorted)
     })
   })
+
+  // test LoadingPage from "./views/LoadingPage.js" 
+  describe("But it is loading", () => {
+    test("Then Loading page should be rendered", () => {
+      // build user interface
+      document.body.innerHTML = BillsUI({ loading: true })
+      // the loading page shoud load
+      expect(screen.getAllByText("Loading...")).toBeTruthy()
+    })
+  })
+
+   // test ErrorPage from "./views/ErrorPage.js"
+  describe("But an error occured", () => {
+    test("Then Error page should be rendered", () => {
+      // build user interface
+      document.body.innerHTML = BillsUI({ error: "an error occured" })
+      // the error page should load
+      expect(screen.getAllByText("Erreur")).toBeTruthy()
+    })
+  })
+
+  describe("And I click on the new bill button", () => {
+    test("Then the click new bill handler should be called", () => {  
+      const onNavigate = () => {return}
+      // build user interface
+      document.body.innerHTML = BillsUI({ data: bills })
+      // Bills for test
+      const billsTestNewBillBtn = new Bills({ document, onNavigate, store: null, localStorage: window.localStorage })
+      // Mock handleClickNewBill
+      billsTestNewBillBtn.handleClickNewBill = jest.fn()
+      // Add event and click action
+      screen.getByTestId("btn-new-bill").addEventListener("click", billsTestNewBillBtn.handleClickNewBill)
+      screen.getByTestId("btn-new-bill").click()
+      // handleClickNewBill should be called
+      expect(billsTestNewBillBtn.handleClickNewBill).toBeCalled()
+    })
+  })
+
+  // test click on eye
+  describe("And I click on the eye icon", () => {
+    
+    test("A modal should open", () => {
+      // build user interface
+      document.body.innerHTML = BillsUI({ data: bills })
+      // Bills for test
+      const billsTestModal = new Bills({ document, onNavigate: null, store: null, localStorage: window.localStorage })
+      billsTestModal.handleClickIconEye = jest.fn()
+
+      screen.getAllByTestId("icon-eye")[0].click()
+
+      expect(billsTestModal.handleClickIconEye).toBeCalled()
+    })
+    test("Then the modal should display the attached image", () => {
+      // build user interface
+      document.body.innerHTML = BillsUI({ data: bills })
+      
+      // Bills for test
+      const billsTestModal = new Bills({ document:document, onNavigate: null, store: null, localStorage: window.localStorage })
+      
+      // Eye icon in DOM
+      const iconEye = document.querySelector(`div[data-testid="icon-eye"]`)
+      
+      // Mock modal behavior
+      $.fn.modal = jest.fn()
+
+      // Mock function handleClickIconEye
+      billsTestModal.handleClickIconEye(iconEye)
+
+      // handleClickIconEye function must be called
+      expect($.fn.modal).toBeCalled()
+      
+      // The modal must be present
+      expect(document.getElementById('modaleFile')).toBeTruthy()
+    })
+  })
+
 })
